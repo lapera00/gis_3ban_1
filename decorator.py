b727d3db29aa18b6da36f04f85b4e0981241dfa5
leeseungjin <pelor7317@likelion.org>
2021-07-21 16:15:08 +0900

    7/21 3ban class commit 4

--- decorator.py
+++ decorator.py
@@ -13,3 +13,28 @@ def hello_world(input_text):
 
 
 hello_world('hello World!')
+
+print("\n\n")
+
+
+
+def decorator1(func):
+    def decorated1(a, h):
+        if a > 0 and h > 0:
+            func(a, h)
+        else:
+            ValueError('error')
+    return decorated1
+
+@decorator1
+def sam(a, h):
+    print(a * h / 2)
+
+@decorator1
+def sag(a, h):
+    print(a * h)
+
+
+
+sam(4, 5)
+sag(4, 4)
